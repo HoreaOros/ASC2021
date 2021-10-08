532271c9fd0c77a9f87cadc7c1b7dc4b151819f3
Horea Oros <horea.oros@gmail.com>
2021-10-08 08:12:31 +0300

    add floating point demo rounding error

--- DemoHelloWorld/DemoHelloWorld/Program.cs
+++ DemoHelloWorld/DemoHelloWorld/Program.cs
@@ -11,6 +11,23 @@ namespace DemoHelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World of C#!");
+
+            double c = 0.001;
+            double suma = 0;
+            double epsilon = 0.000001;
+            for (int i = 0; i < 1000000; i++)
+            {
+                suma += c;
+            }
+
+            if (Math.Abs(suma - 1000) < epsilon)
+            {
+                Console.WriteLine("Detonte bomb!");
+            }
+            else
+            {
+                Console.WriteLine("Phew!");
+            }
         }
     }
 }
